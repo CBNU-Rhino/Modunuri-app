793840f344f2aba37ca3e33205ee13eca015e132
yangwoohyeon <dngus0717@gmail.com>
2024-11-14 18:52:22 +0900

    카카오 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,13 @@ dependencies {
     implementation ("com.google.android.material:material:1.9.0")  // 또는 최신 버전
     implementation ("com.squareup.picasso:picasso:2.71828")
     implementation ("com.github.bumptech.glide:glide:4.15.1") // 최신 버전 확인 후 사용
-
+    implementation ("com.kakao.sdk:v2-all:2.20.6") // 전체 모듈 설치, 2.11.0 버전부터 지원
+    implementation ("com.kakao.sdk:v2-user:2.20.6") // 카카오 로그인 API 모듈
+    implementation ("com.kakao.sdk:v2-share:2.20.6") // 카카오톡 공유 API 모듈
+    implementation ("com.kakao.sdk:v2-talk:2.20.6") // 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation ("com.kakao.sdk:v2-friend:2.20.6") // 피커 API 모듈
+    implementation ("com.kakao.sdk:v2-navi:2.20.6") // 카카오내비 API 모듈
+    implementation ("com.kakao.sdk:v2-cert:2.20.6") // 카카오톡 인증 서비스 API 모듈
     implementation(libs.retrofit)               // Retrofit
     implementation(libs.converter.gson)          // Gson converter
 }
